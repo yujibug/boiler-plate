e14536f16a741d7e352454592b90965742ea0c14
gimyuji476@gmail.com <gimyuji476@gmail.com>
2021-05-07 08:07:45 +0900

    bodyParse linethrough 문제발생

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
 mongoose
@@ -18,6 +22,13 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/register', (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User();
 });
 
 app.listen(port, () => {
